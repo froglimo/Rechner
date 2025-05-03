ac8bfd03dd94d3b8d9c87bcfb30996f1fe463022
froglimo <melvis@posteo.de>
2025-05-03 13:18:23 +0200

    Add files via upload

---
+++ Rechner.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int f, e;
+
+    // Benutzereingabe
+    printf("Gib die erste ganze Zahl ein: ");
+    scanf("%d", &f);
+
+    printf("Gib die zweite ganze Zahl ein: ");
+    scanf("%d", &e);
+
+    // Berechnungen
+    int addieren = f + e;
+    int subtrahieren = f - e;
+    int multiplizieren = f * e;
+
+    // Division mit Überprüfung auf Division durch Null
+    if (e != 0) {
+        float dividieren = (float)f / e;
+        printf("\nErgebnisse der Berechnungen:\n");
+        printf("Addition: %d + %d = %d\n", f, e, addieren);
+        printf("Subtraktion: %d - %d = %d\n", f, e, subtrahieren);
+        printf("Multiplikation: %d * %d = %d\n", f, e, multiplizieren);
+        printf("Division: %d / %d = %.2f\n", f, e, dividieren);
+    } else {
+        printf("\nErgebnisse der Berechnungen:\n");
+        printf("Addition: %d + %d = %d\n", f, e, addieren);
+        printf("Subtraktion: %d - %d = %d\n", f, e, subtrahieren);
+        printf("Multiplikation: %d * %d = %d\n", f, e, multiplizieren);
+        printf("Division: Division durch Null ist nicht erlaubt.\n");
+    }
+
+    return 0;
+}
